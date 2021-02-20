40ddb497087fa75d4a5543b559a6acfbca366f31
hhn17 <68228130+hhn17@users.noreply.github.com>
2021-02-19 23:49:03 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ from news_ingester import*
 import pytest
 
 def test_NLP():
-  assert NLP_analysis("") == "301-file does not exist"
+    assert NLP_analysis("") == "301-file does not exist"
 
 def test_file():
-  assert Create("A1",123,"SS") == "100-create successful"
-  assert Update("A1",123,"SS") =="110-update successful"
+    assert Create("A1",123,"SS") == "100-create successful"
+    assert Update("A1",123,"SS") =="110-update successful"
   
 def test_new():
-  assert new_ingester("")=="201-link not found"
+    assert new_ingester("")=="201-link not found"
