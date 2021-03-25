0dabb8b957a45bd94c0a8764b98dde6af09efc28
hhn17 <68228130+hhn17@users.noreply.github.com>
2021-03-25 14:42:02 -0400

    Update util.py

--- File Uploader/util.py
+++ File Uploader/util.py
@@ -5,9 +5,7 @@ import config
 import json
 
 def get_sentiment(cont):
-    """
-    Fetch the sentiment score from api created for google nlp
-    """
+    
     data = dict()
     data['content'] = cont
     header_conf = {'Content-type': 'application/json', 'Accept': 'text/plain'}
