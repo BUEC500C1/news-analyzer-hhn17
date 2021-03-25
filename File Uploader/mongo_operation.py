1fb9d8f47e083e4f3d0db39c6a3d8d25110d4ded
hhn17 <68228130+hhn17@users.noreply.github.com>
2021-03-25 14:41:50 -0400

    Update mongo_operation.py

--- File Uploader/mongo_operation.py
+++ File Uploader/mongo_operation.py
@@ -5,9 +5,7 @@ import logging
 from bson.objectid import ObjectId
 
 def get_db_conn():
-    """
-    Fetch the mongodb connection 
-    """
+    
     try:
         myclient = pymongo.MongoClient(config.monog_address)
     except Exception as exp:
@@ -17,9 +15,7 @@ def get_db_conn():
     return mydb
 
 def search(data, client_db):
-    """
-    search function for mongodb client
-    """
+    
     mycol = client_db["textmaintainer"]
     myquery = {"_id": ObjectId('6039e735a535f959e38d1bda')}
     mydoc = mycol.find(myquery)
@@ -29,18 +25,13 @@ def search(data, client_db):
     return count
     
 def insert_record(data, client_db):
-    """
-    inset record in mongodb database 
-    """
+    
     mycol = client_db["textmaintainer"]
     x = mycol.insert_one(data)
     return x
 
 def update_record(data, client_db):
-    """
-    update the record with doc_id and
     
-    """
     mycol = client_db["textmaintainer"]
     #count = search(data, client_db)
     #if count == 1:
@@ -51,15 +42,13 @@ def update_record(data, client_db):
     return result
 
 def  delete_record(data, mg_client):
-    """
-    delete record from database
-    """
+    
     mycol = mg_client["textmaintainer"]
     myquery = data
     logging.debug(myquery)
     result = mycol.delete_one(myquery)
     
-    # print the API call's results
+    
     logging.debug("API call recieved:".format(result.acknowledged))
     logging.debug("Documents deleted:".format(result.deleted_count))
     return result
