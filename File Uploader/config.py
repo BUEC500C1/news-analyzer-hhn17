d2d6899ce0c54c6108ce1555d4107a8747e65a40
hhn17 <68228130+hhn17@users.noreply.github.com>
2021-04-04 01:37:19 -0400

    Delete File Uploader directory

--- File Uploader/config.py
+++
@@ -1,4 +0,0 @@
-import os
-db_name = os.environ.get("DB_NAME","clientDB")
-monog_address = os.environ.get("MONGO_ADDRESS","mongodb://localhost:27017/")
-sentiment_api = os.environ.get("SENTIMENT_API","http://localhost:8118/api/v1/getSentiment")
